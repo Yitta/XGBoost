4f209ba2774cc386f4d49658a5c767c4d4a6fb1a
Ying <12481747@student.uts.edu.au>
2018-09-18 22:01:41 +1000

    modelling

---
+++ training.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Author:   Ying
+    Date:     2018/09
+"""
+
+import numpy as np
+import pandas as pd
+import constant
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+
